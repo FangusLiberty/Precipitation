8d23bf11ef328963ca6c4cf33b3b52d2ed8cc783
FangusLiberty <eduardcccignatovich@gmail.com>
2022-01-18 18:03:11 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #3. Precipitation\n";
     cout << "Author: Eduard Ignatovich\n";
+    cout << "Group: 14\n";
     return 0;
 }
